4ac99c035e75e068f42ef558e762ef11f311a27f
Rakesh Rengaraj <rakesh@ptotem.com>
2016-09-14 16:41:44 +0530

    hoppipola quizspin

--- assets/js/standardLoad/adaptor/modules/quiz/question-bank.js
+++ assets/js/standardLoad/adaptor/modules/quiz/question-bank.js
@@ -1,266 +1,266 @@
 var questionbank = {};
 questionbank.sequentialQuestions = 4;
-questionbank.questionsFromTopic = 8;
+questionbank.questionsFromTopic = 4;
 questionbank.questions = [
     {
-        statement: "The British dish Scotch egg may have been inspired by the Mughlai dish ____________ ______, where hard-boiled eggs are encased in a layer of spicy kofta meat.",
-        img: "assets/img/subway/Slide17.JPG",
+        statement: "Hoppipola was established in ________.",
+        img: "assets/img/hoppipola/Slide1.PNG",
         topic: 'basic',
         weight: 1,
 
-        opta: "Egg Chop",
+        opta: "2015",
         optacorrect: false,
         optapoints: "0",
 
-        optb: "Nargisi Kofta",
+        optb: "2014",
         optbcorrect: true,
         optbpoints: "10",
 
-        optc: "Plantain Kofta",
+        optc: "2013",
         optccorrect: false,
         optcpoints: "0",
 
-        optd: "Malai Kofta",
+        optd: "2012",
         optdcorrect: false,
         optdpoints: "0",
 
         Slide_id:321
     },
     {
-        statement: "Sutli Kebab is mostly famous in ________.",
-        img: "assets/img/subway/Slide15.JPG",
+        statement: "Hoppipola is one of the Specilaity Restaurants.",
+        img: "assets/img/hoppipola/Slide2.PNG",
         topic: 'basic',
         weight: 1,
 
-        opta: "Hyderabad",
+        opta: "False",
         optacorrect: false,
         optapoints: "0",
 
-        optb: "Bangladesh",
+        optb: "True",
         optbcorrect: true,
         optbpoints: "10",
 
-        optc: "Lahore",
+        optc: "May be True",
         optccorrect: false,
         optcpoints: "0",
 
-        optd: "Mumbai",
+        optd: "May be False",
         optdcorrect: false,
         optdpoints: "0",
 
         Slide_id:321
     },
    {
-       statement: "______ kebab is considered as the oldest version of kebabs.",
-       img: "assets/img/subway/Slide14.JPG",
+       statement: "The tagline of Hoppipola is ________.",
+       img: "assets/img/hoppipola/Slide3.PNG",
        topic: 'basic',
        weight: 1,
 
-       opta: "Seekh",
+       opta: "Play. Eat. Drink.",
        optacorrect: false,
        optapoints: "0",
 
-       optb: "Shish",
+       optb: "Eat. Drink. Play.",
        optbcorrect: true,
        optbpoints: "10",
 
-       optc: "Bihari",
+       optc: "Drink. Eat. Play.",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "Kasturi",
+       optd: "Eat. Play. Drink.",
        optdcorrect: false,
        optdpoints: "0",
 
        Slide_id:321
    },
    {
-       statement: "Reshmi kebab is influenced by the ________.",
-       img: "assets/img/subway/Slide12.JPG",
+       statement: "You never shake a Martini, you stir it.",
+       img: "assets/img/hoppipola/Slide4.PNG",
        topic: 'basic',
        weight: 1,
 
-       opta: "Mughals",
+       opta: "True",
        optacorrect: true,
        optapoints: "10",
 
-       optb: "Greeks",
+       optb: "False",
        optbcorrect: false,
        optbpoints: "0",
 
-       optc: "Turks",
+       optc: "May be true",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "None of the above",
+       optd: "May be false",
        optdcorrect: false,
        optdpoints: "0",
 
        Slide_id:321
    },
    {
-       statement: "______ kebab was originated in the camps of Arab and Turkish invaders who used to roast meat chunks skewered on their sword edges.",
-       img: "assets/img/subway/Slide1.JPG",
-       weight: 1,
-       topic: 'basic',
-
-       opta: "Shish",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Bihari",
-       optbcorrect: true,
-       optbpoints: "10",
-
-       optc: "Tunde",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Seekh",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "The number of spices the original recipe of Tunde kebab contained was ____.",
-       img: "assets/img/subway/Slide16.JPG",
-       weight: 1,
-       topic: 'basic',
-
-       opta: "160",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "10",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "4",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Which kebab gets its name from the fact that its creator had a missing arm?",
-       img: "assets/img/subway/Slide16.JPG",
-       weight: 1,
-       topic: 'basic',
-
-       opta: "Tunde Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Hariyali Kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Bihari Kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Seekh Kebab",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "__________, the famous traveller has mentioned that Kebab was an integral part of the daily diet of Indian royalty as early as 1200 AD.",
-       weight: 1,
-       img: "assets/img/subway/Slide9.JPG",
-       topic: 'basic',
-
-       opta: "Ibn Battuta",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Vitus Berring",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Heman Cortes",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Vasco da Gama",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Galawati kebab is also known as ________.",
-       img: "assets/img/subway/Slide6.JPG",
-       weight: 1,
-       topic: 'basic',
-
-       opta: "Shawarma",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Gilawati kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Tunde kebab",
-       optccorrect: true,
-       optcpoints: "10",
-
-       optd: "Shish kebab",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Shami kebab is a popular kebab in _________.",
-       img: "assets/img/subway/Slide13.JPG",
+       statement: "The MARGARITA drink was named after the person who first tasted it",
+       img: "assets/img/hoppipola/Slide5.PNG",
        weight: 1,
        topic: 'intermediate',
 
-       opta: "India",
+       opta: "False",
        optacorrect: false,
        optapoints: "0",
 
-       optb: "Pakistan",
-       optbcorrect: false,
-       optbpoints: "0",
+       optb: "True",
+       optbcorrect: true,
+       optbpoints: "10",
 
-       optc: "Bangladesh",
+       optc: "May be true",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "All the above",
+       optd: "May be false",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },
+   {
+       statement: "Which term was coined first- Mocktail or Cocktail?",
+       img: "assets/img/hoppipola/Slide6.PNG",
+       weight: 1,
+       topic: 'intermediate',
+
+       opta: "Cocktail",
+       optacorrect: true,
+       optapoints: "10",
+
+       optb: "Mocktail",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "Cannot predict",
+       optccorrect: false,
+       optcpoints: "0",
+
+       optd: "No idea",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },
+   {
+       statement: "White Wine can be produced both by Red and Green Grapes.",
+       img: "assets/img/hoppipola/Slide7.PNG",
+       weight: 1,
+       topic: 'intermediate',
+
+       opta: "True",
+       optacorrect: true,
+       optapoints: "10",
+
+       optb: "False",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "May be true",
+       optccorrect: false,
+       optcpoints: "0",
+
+       optd: "May be false",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },
+   {
+       statement: "Mojos are basically potato wedges.",
+       weight: 1,
+       img: "assets/img/hoppipola/Slide8.PNG",
+       topic: 'intermediate',
+
+       opta: "True",
+       optacorrect: true,
+       optapoints: "10",
+
+       optb: "False",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "May be true",
+       optccorrect: false,
+       optcpoints: "0",
+
+       optd: "May be false",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },
+   {
+       statement: "Speciality Restaurants is the first and the only public listed company in the fine dining restaurant category.",
+       img: "assets/img/hoppipola/Slide9.PNG",
+       weight: 1,
+       topic: 'Advanced',
+
+       opta: "False",
+       optacorrect: false,
+       optapoints: "0",
+
+       optb: "May be true",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "True",
+       optccorrect: true,
+       optcpoints: "10",
+
+       optd: "May be false",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },
+   {
+       statement: "The oldest wine cellar in the world is situated in?",
+       img: "assets/img/hoppipola/Slide10.PNG",
+       weight: 1,
+       topic: 'Advanced',
+
+       opta: "No idea",
+       optacorrect: false,
+       optapoints: "0",
+
+       optb: "Europe",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "Rome",
+       optccorrect: false,
+       optcpoints: "0",
+
+       optd: "Titanic",
        optdcorrect: true,
        optdpoints: "10",
 
        Slide_id:321
    },
    {
-       statement: "Doner Kebab is of Turkish origin and is similar to _________.",
-       img: "assets/img/subway/Slide5.JPG",
+       statement: "The Country with the highest per Capita Wine consumption in the World is _________.",
+       img: "assets/img/hoppipola/Slide11.PNG",
        weight: 1,
-       topic: 'intermediate',
+       topic: 'Advanced',
 
-       opta: "Shawarma",
+       opta: "Vatican City",
        optacorrect: true,
        optapoints: "10",
 
-       optb: "Chicken lollipop",
+       optb: "Rome",
        optbcorrect: false,
        optbpoints: "0",
 
-       optc: "Tangdi kebab",
+       optc: "Italy",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "Chicken tikka",
+       optd: "None of the above",
        optdcorrect: false,
        optdpoints: "0",
 
@@ -268,367 +268,30 @@ questionbank.questions = [
    }
     ,
    {
-       statement: "Strings are used in tying this kebab to the skewer because the pieces of meat are so soft that they tend to fall down. Which kebab is this?",
-       img: "assets/img/subway/Slide15.JPG",
+       statement: "Nachos were named after the person who invented them.",
+       img: "assets/img/hoppipola/Slide12.PNG",
        weight: 1,
-       topic: 'intermediate',
+       topic: 'Advanced',
 
-       opta: "Shish kebab",
+       opta: "False",
        optacorrect: false,
        optapoints: "0",
 
-       optb: "Shami kebab",
+       optb: "May be true",
        optbcorrect: false,
        optbpoints: "0",
 
-       optc: "Sutli kebab",
+       optc: "True",
        optccorrect: true,
        optcpoints: "10",
 
-       optd: "None of the above",
+       optd: "May be false",
        optdcorrect: false,
        optdpoints: "0",
 
        Slide_id:321
    },
-   {
-       statement: "Shikhampur kebab is also known as _________________.",
-       weight: 1,
-       img: "assets/img/subway/Slide19.JPG",
-       topic: 'intermediate',
-
-       opta: "Phool ke kebab",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Patthar ke kebab",
-       optbcorrect: true,
-       optbpoints: "10",
-
-       optc: "Malai kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Shikhampur or Patthar ke kebab was originally made under the patronage of the Nizam by heating the stone on which it is placed.",
-       img: "assets/img/subway/Slide19.JPG",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "True",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "False",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Maybe",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Not A ",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Chapli kebab is called chapli because of it's flat look.",
-       img: "assets/img/subway/Slide3.JPG",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "True",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "False",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Maybe",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Not Applicable",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Chaprikh is a Pashto word meaning flat. A kebab gets its name from a derivation of this word. Which kebab is this?",
-       img: "assets/img/subway/Slide3.JPG",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "Chapli Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Kasturi Kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Hariyali Kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-
-   {
-       statement: "Kakori Kabab is one of the most famous dishes of _______ cuisine.",
-       weight: 1,
-       img: "assets/img/subway/Slide8.JPG",
-       topic: 'intermediate',
-
-       opta: "Awadhi",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Mughlai",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Hyderabadi",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Chettinad",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "India was traditionally the birthplace of kebabs.",
-       img: "assets/img/subway/Slide11.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "True",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "False",
-       optbcorrect: true,
-       optbpoints: "0",
-
-       optc: "Maybe",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Not A ",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Which country are kebabs originally from?",
-       img: "assets/img/subway/Slide10.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Turkey",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Saudi Arabia",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Pakistan",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Which food item was created when Turkish soldiers grilled chunks of freshly hunted animals skewed on swords on open field fires.",
-       img: "assets/img/subway/Slide11.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Pasta",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Sushi",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-       {
-       statement: "This kebab, made from chickpeas, chicken and eggs, was created in the Mughal kitchens by Syrian chefs.",
-       img: "assets/img/subway/Slide17.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Shami Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Boti Kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Reshmi Kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Falafel",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-
-   {
-       statement: "This kebab was created for Nawab Wajid Ali Shah who wanted to eat kebabs but had no teeth.",
-       weight: 1,
-       img: "assets/img/subway/Slide7.JPG",
-       topic: 'Advanced',
-
-       opta: "Galawati Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Tangdi Kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Boti Kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "A family of meatball dishes, this food is eaten across South Asian. and Middle Eastern cuisine. A vegetarian version of this in curry is eaten across India.",
-       img: "assets/img/subway/Slide18.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Falafel",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Kofta",
-       optbcorrect: true,
-       optbpoints: "0",
-
-       optc: "Pakoda",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Not Applicable",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "This kebab was created for Nawab Wajid Ali Shah who wanted to eat kebabs but had no teeth.",
-       img: "assets/img/subway/Slide6.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Galawati Kebab",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Tangdi Kebab",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Boti Kebab",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "The word kebab is said to originate from the ______ language.",
-       img: "assets/img/subway/Slide9.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Persian",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Turks",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Central Asians",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Arabic",
-       optdcorrect: true,
-       optdpoints: "10",
-
-       Slide_id:321
-   },
-   {
-       statement: "The variation of the modern day Bihari kebab is a purely....",
-       img: "assets/img/subway/Slide2.JPG",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "Bihari and Bengali take on the meat.",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Bihari and Marathi take on the meat.",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Bihari and Assamese take on the meat.",   
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "None of the above",
-       optdcorrect: true,
-       optdpoints: "10",
-
-       Slide_id:321
-   }  
+   
 ];
 
 //questionbank.questions=parent.getQuestionsFromBank(parent.currentIntegratedGame);
