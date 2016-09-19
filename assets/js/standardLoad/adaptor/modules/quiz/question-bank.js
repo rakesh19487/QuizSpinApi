70e2ca46582a24ea7882b4cdd5a6af2195d6c192
Rakesh Rengaraj <rakesh@ptotem.com>
2016-09-19 14:48:48 +0530

    marketing assets

--- assets/js/standardLoad/adaptor/modules/quiz/question-bank.js
+++ assets/js/standardLoad/adaptor/modules/quiz/question-bank.js
@@ -3,295 +3,125 @@ questionbank.sequentialQuestions = 4;
 questionbank.questionsFromTopic = 4;
 questionbank.questions = [
     {
-        statement: "Hoppipola was established in ________.",
-        img: "assets/img/hoppipola/Slide1.png",
+        statement: "A method of money laundering in which cash is broken into smaller deposits of money, used to defeat suspicion of money laundering.",
+        img: "assets/img/KnowMore/KnowMore1.png",
         topic: 'basic',
         weight: 1,
 
-        opta: "2015",
-        optacorrect: false,
-        optapoints: "0",
+        opta: "Smurfing/Structuring",
+        optacorrect: true,
+        optapoints: "10",
 
-        optb: "2014",
-        optbcorrect: true,
-        optbpoints: "10",
+        optb: "Trade business laundering",
+        optbcorrect: false,
+        optbpoints: "0",
 
-        optc: "2013",
+        optc: "Round Tripping",
         optccorrect: false,
         optcpoints: "0",
 
-        optd: "2012",
+        optd: "Fictitious Loans",
         optdcorrect: false,
         optdpoints: "0",
 
         Slide_id:321
     },
     {
-        statement: "Hoppipola is one of the Specilaity Restaurants.",
-        img: "assets/img/hoppipola/Slide2.png",
+        statement: "A process that disguises a legitimate source of funds that are to be used for illegal purposes.",
+        img: "assets/img/KnowMore/KnowMore2.png",
         topic: 'basic',
         weight: 1,
 
-        opta: "False",
+        opta: "Terrorism Financing",
         optacorrect: false,
         optapoints: "0",
 
-        optb: "True",
-        optbcorrect: true,
-        optbpoints: "10",
+        optb: "Money Laundering",
+        optbcorrect: false,
+        optbpoints: "0",
 
-        optc: "May be True",
-        optccorrect: false,
-        optcpoints: "0",
+        optc: "Reverse Money Laundering",
+        optccorrect: true,
+        optcpoints: "10",
 
-        optd: "May be False",
+        optd: "Bulk cash smuggling",
         optdcorrect: false,
         optdpoints: "0",
 
         Slide_id:321
     },
    {
-       statement: "The tagline of Hoppipola is ________.",
-       img: "assets/img/hoppipola/Slide3.png",
+       statement: "Under or overvaluing invoices to disguise the movement of money is…?",
+       img: "assets/img/KnowMore/KnowMore3.png",
        topic: 'basic',
        weight: 1,
 
-       opta: "Play. Eat. Drink.",
+       opta: "Black Salaries",
        optacorrect: false,
        optapoints: "0",
 
-       optb: "Eat. Drink. Play.",
-       optbcorrect: true,
+       optb: "Bank Capture",
+       optbcorrect: false,
        optbpoints: "10",
 
-       optc: "Drink. Eat. Play.",
+       optc: "Bulk Cash Smuggling",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "Eat. Play. Drink.",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "You never shake a Martini, you stir it.",
-       img: "assets/img/hoppipola/Slide4.png",
-       topic: 'basic',
-       weight: 1,
-
-       opta: "True",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "False",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "May be true",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "May be false",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "The MARGARITA drink was named after the person who first tasted it",
-       img: "assets/img/hoppipola/Slide5.png",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "False",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "True",
-       optbcorrect: true,
-       optbpoints: "10",
-
-       optc: "May be true",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "May be false",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Which term was coined first- Mocktail or Cocktail?",
-       img: "assets/img/hoppipola/Slide6.png",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "Cocktail",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "Mocktail",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Cannot predict",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "No idea",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "White Wine can be produced both by Red and Green Grapes.",
-       img: "assets/img/hoppipola/Slide7.png",
-       weight: 1,
-       topic: 'intermediate',
-
-       opta: "True",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "False",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "May be true",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "May be false",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Mojos are basically potato wedges.",
-       weight: 1,
-       img: "assets/img/hoppipola/Slide8.png",
-       topic: 'intermediate',
-
-       opta: "True",
-       optacorrect: true,
-       optapoints: "10",
-
-       optb: "False",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "May be true",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "May be false",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "Speciality Restaurants is the first and the only public listed company in the fine dining restaurant category.",
-       img: "assets/img/hoppipola/Slide9.png",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "False",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "May be true",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "True",
-       optccorrect: true,
-       optcpoints: "10",
-
-       optd: "May be false",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   },
-   {
-       statement: "The oldest wine cellar in the world is situated in?",
-       img: "assets/img/hoppipola/Slide10.png",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "No idea",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "Europe",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "Rome",
-       optccorrect: false,
-       optcpoints: "0",
-
-       optd: "Titanic",
+       optd: "Trade Based Laundering",
        optdcorrect: true,
        optdpoints: "10",
 
        Slide_id:321
    },
    {
-       statement: "The Country with the highest per Capita Wine consumption in the World is _________.",
-       img: "assets/img/hoppipola/Slide11.png",
+       statement: "What does FATF stand for?",
+       img: "assets/img/KnowMore/KnowMore4.png",
+       topic: 'basic',
        weight: 1,
-       topic: 'Advanced',
 
-       opta: "Vatican City",
+       opta: "Financial Action Task Force",
        optacorrect: true,
        optapoints: "10",
 
-       optb: "Rome",
+       optb: "Finance Activities Task Force",
        optbcorrect: false,
        optbpoints: "0",
 
-       optc: "Italy",
+       optc: "Finance Activity Task Forum",
        optccorrect: false,
        optcpoints: "0",
 
-       optd: "None of the above",
-       optdcorrect: false,
-       optdpoints: "0",
-
-       Slide_id:321
-   }
-    ,
-   {
-       statement: "Nachos were named after the person who invented them.",
-       img: "assets/img/hoppipola/Slide12.png",
-       weight: 1,
-       topic: 'Advanced',
-
-       opta: "False",
-       optacorrect: false,
-       optapoints: "0",
-
-       optb: "May be true",
-       optbcorrect: false,
-       optbpoints: "0",
-
-       optc: "True",
-       optccorrect: true,
-       optcpoints: "10",
-
-       optd: "May be false",
+       optd: "Financial Action Task Forum",
        optdcorrect: false,
        optdpoints: "0",
 
        Slide_id:321
    },
-   
+   {
+       statement: "In which year did the Parliament of India pass the Prevention of Money Laundering Act?",
+       img: "assets/img/KnowMore/KnowMore5.png",
+       weight: 1,
+       topic: 'intermediate',
+
+       opta: "2002",
+       optacorrect: true,
+       optapoints: "10",
+
+       optb: "1989",
+       optbcorrect: false,
+       optbpoints: "0",
+
+       optc: "1994",
+       optccorrect: false,
+       optcpoints: "0",
+
+       optd: "2012",
+       optdcorrect: false,
+       optdpoints: "0",
+
+       Slide_id:321
+   },   
 ];
 
 //questionbank.questions=parent.getQuestionsFromBank(parent.currentIntegratedGame);
